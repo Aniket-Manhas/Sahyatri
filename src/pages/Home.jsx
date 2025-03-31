04f9555417d29f3ad2da79355c5f1980031fe01c
Aniket Manhas <aniketmanhas801@gmail.com>
2025-03-31 20:20:41 +0530

    remove logo path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import logo from "./logo.png";
 export default function Home() {
   const { currentUser } = useAuth();
 
